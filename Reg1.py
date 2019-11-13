e75a0aff0c8e29a023aea18252287bace1d669f8
Chayan Kathuria <chayankathuria@gmail.com>
2019-11-13 10:25:20 +0530

    Added TTS and Polymomial regression

--- Reg1.py
+++ Reg1.py
@@ -52,3 +52,49 @@ for name, model in pipelines:
     names.append(name)
     msg = "%s: %f (%f)" % (name, cv_results.mean()*100, cv_results.std()*100)
     print(msg)
+    
+n_splits=7
+pd.set_option('precision',3) # to set the 3-digits for display
+
+# To Display the result dataframe for all the CVs 0-7
+results_df = pd.DataFrame(results, index=names, \
+                          columns='CV1 CV2 CV3 CV4 CV5 CV6 CV7'.split())
+results_df['CV Mean'] = results_df.iloc[:,0:n_splits].mean(axis=1)
+results_df['CV Std Dev'] = results_df.iloc[:,0:n_splits].std(axis=1)
+
+results_df.sort_values(by='CV Mean', ascending=False)
+
+# Using Test Train Split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(x,y, \
+                                                    test_size=0.3, 
+                                                    random_state=200)
+
+scoring = 'neg_mean_squared_error'
+results = []
+names = []
+for name, model in pipelines:
+    model.fit(X_train,y_train)
+    result = model.score(X_test,y_test)
+    results.append(result)
+    names.append(name)
+    msg = "%s: %f" % (name, result)
+    print(msg)
+
+# Polynomial regression
+PR = PolynomialFeatures(6)
+X_poly = PR.fit_transform(x)
+Poly_model = LinearRegression()
+Poly_model.fit(X_poly,y)
+
+plt.plot(X_train,y_train,'bs')
+plt.plot(X_test,y_test,'ro')
+plt.plot(x,Poly_model.predict(X_poly),'g-*')
+plt.legend(['Train','Test Data', 'Poly4'])
+
+for i in range(1,30):
+    PR = PolynomialFeatures(i)
+    X_poly = PR.fit_transform(x)
+    Poly_model = LinearRegression(fit_intercept=True)
+    Poly_model.fit(X_poly,y)
+    print(i, Poly_model.score(X_poly,y))
